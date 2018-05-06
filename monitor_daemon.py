14451b7f4e590c62e2090be40100adb9ebdd422c
Ben Gould <ben.gould@inovexcorp.com>
2018-05-06 15:17:20 -0400

    sleep at the end of the loop

--- monitor_daemon.py
+++ monitor_daemon.py
@@ -24,15 +24,17 @@ def configure():
 configure()
 
 th = TemperHandler()
+## There is a device discovered by the TemerHandler
 if len(th.get_devices()) == 1:
     dev = th.get_devices()[0]
     sensors = range(dev.get_sensor_count())
     while True:
-        time.sleep(SLEEP_TIME)
         temperatures = dev.get_temperatures(sensors=sensors)
         if(temperatures.get(0).get(TEMPERATURE_ATTR)):
             print temperatures[0][TEMPERATURE_ATTR]
         else:
             raise Exception("Invalid reading!")
+        time.sleep(SLEEP_TIME)
+## No devices found...
 else:
     print "No USB temperature device found!"
