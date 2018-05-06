e2ad8798a67069e3e1a9d4b0b6e5b7a9d5e5cbde
Ben Gould <ben.gould@inovexcorp.com>
2018-05-06 15:09:40 -0400

    python start for monitoring

---
+++ monitor_daemon.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import sys
+import os
+import time
+import math
+import logging
+import math
+import yaml
+from temperusb.temper import TemperHandler, TemperDevice
+
+SLEEP_TIME=1
+TEMPERATURE_ATTR="temperature_f"
+
+def configure():
+    global SLEEP_TIME, TEMPERATURE_ATTR
+    conf_file = sys.argv[1]
+    with open(conf_file, 'r') as stream:
+        config_yaml = yaml.load(stream)
+    SLEEP_TIME=config_yaml["sleep_time"] if config_yaml.get("sleep_time")
+    TEMPERATURE_ATT=config_yaml["temperature_attr"] if config_yaml.get("temperature_attr")
+
+## Configure the daemon process
+configure()
+
+th = TemperHandler()
+if len(th.get_devices()) == 1:
+    dev = th.get_devices()[0]
+    sensors = range(dev.get_sensor_count())
+    while True:
+        time.sleep(SLEEP_TIME)
+        temperatures = dev.get_temperatures(sensors=sensors)
+        if(temperatures.get(0).get(TEMPERATURE_ATTR)):
+            print temperatures[0][TEMPERATURE_ATTR]
+        else:
+            raise Exception("Invalid reading!")
+else:
+    print "No USB temperature device found!"
